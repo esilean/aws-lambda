dda11a1f235e9948fee5aeae6e25514716a17690
Leandro Bevilaqua <le.bevilaqua@gmail.com>
2020-12-24 12:14:53 -0300

    lamb

---
+++ LambdaSNS/test/LambdaSNS.Tests/FunctionTest.cs
@@ -0,0 +1,40 @@
+using Amazon.Lambda.SNSEvents;
+using Amazon.Lambda.TestUtilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LambdaSNS.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public async Task TestSQSEventLambdaFunction()
+        {
+            var snsEvent = new SNSEvent
+            {
+                Records = new List<SNSEvent.SNSRecord>
+                {
+                    new SNSEvent.SNSRecord
+                    {
+                        Sns = new SNSEvent.SNSMessage()
+                        {
+                            Message = "foobar"
+                        }
+                    }
+                }
+            };
+
+            var logger = new TestLambdaLogger();
+            var context = new TestLambdaContext
+            {
+                Logger = logger
+            };
+
+            var function = new Function();
+            await function.FunctionHandler(snsEvent, context);
+
+            Assert.Contains("Processed record foobar", logger.Buffer.ToString());
+        }
+    }
+}
